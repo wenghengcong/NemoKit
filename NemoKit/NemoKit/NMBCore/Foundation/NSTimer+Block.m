f17b556093f2858fb3aec1a3749b2a504b6135d0
翁恒丛 <wenghengcong@icloud.com>
2020-06-16 05:46:15 +0800

    add

---
+++ NemoKit/NemoKit/NMBCore/Foundation/NSTimer+Block.m
@@ -0,0 +1,29 @@
+
+//
+//  NSTimer+Block.m
+//  NSTimerBestPractice
+//
+//  Created by Hunt on 2019/7/10.
+//  Copyright © 2019 WengHengcong. All rights reserved.
+//
+
+#import "NSTimer+Block.h"
+
+@implementation NSTimer (Block)
+
+
++ (void)_nm_ExecBlock:(NSTimer *)timer {
+    if ([timer userInfo]) {
+        void (^block)(NSTimer *timer) = (void (^)(NSTimer *timer))[timer userInfo];
+        block(timer);
+    }
+}
+
++ (NSTimer *)nm_scheduledTimerWithTimeInterval:(NSTimeInterval)seconds block:(void (^)(NSTimer *timer))block repeats:(BOOL)repeats {
+    return [NSTimer scheduledTimerWithTimeInterval:seconds target:self selector:@selector(_yy_ExecBlock:) userInfo:[block copy] repeats:repeats];
+}
+
++ (NSTimer *)nm_timerWithTimeInterval:(NSTimeInterval)seconds block:(void (^)(NSTimer *timer))block repeats:(BOOL)repeats {
+    return [NSTimer timerWithTimeInterval:seconds target:self selector:@selector(_nm_ExecBlock:) userInfo:[block copy] repeats:repeats];
+}
+@end
